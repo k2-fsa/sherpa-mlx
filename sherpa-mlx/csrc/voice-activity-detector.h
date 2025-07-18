711ec88b359452743944c681beb03beec92d18b0
Fangjun Kuang <csukuangfj@gmail.com>
2025-07-18 22:10:02 +0800

    Add VAD

---
+++ sherpa-mlx/csrc/voice-activity-detector.h
@@ -0,0 +1,62 @@
+// sherpa-mlx/csrc/voice-activity-detector.h
+//
+// Copyright (c)  2023-2025  Xiaomi Corporation
+
+#ifndef SHERPA_MLX_CSRC_VOICE_ACTIVITY_DETECTOR_H_
+#define SHERPA_MLX_CSRC_VOICE_ACTIVITY_DETECTOR_H_
+
+#include <memory>
+#include <vector>
+
+#include "sherpa-mlx/csrc/vad-model-config.h"
+
+namespace sherpa_mlx {
+
+struct SpeechSegment {
+  int32_t start;  // in samples
+  std::vector<float> samples;
+};
+
+class VoiceActivityDetector {
+ public:
+  explicit VoiceActivityDetector(const VadModelConfig &config,
+                                 float buffer_size_in_seconds = 60);
+
+  template <typename Manager>
+  VoiceActivityDetector(Manager *mgr, const VadModelConfig &config,
+                        float buffer_size_in_seconds = 60);
+
+  ~VoiceActivityDetector();
+
+  void AcceptWaveform(const float *samples, int32_t n);
+  bool Empty() const;
+  void Pop();
+  void Clear();
+
+  // It is an error to call Front() if Empty() returns true.
+  //
+  // The returned reference is valid until the next call to any
+  // methods of VoiceActivityDetector.
+  const SpeechSegment &Front() const;
+
+  bool IsSpeechDetected() const;
+
+  // It is empty if IsSpeechDetected() returns false
+  SpeechSegment CurrentSpeechSegment() const;
+
+  void Reset() const;
+
+  // At the end of the utterance, you can invoke this method so that
+  // the last speech segment can be detected.
+  void Flush() const;
+
+  const VadModelConfig &GetConfig() const;
+
+ private:
+  class Impl;
+  std::unique_ptr<Impl> impl_;
+};
+
+}  // namespace sherpa_mlx
+
+#endif  // SHERPA_MLX_CSRC_VOICE_ACTIVITY_DETECTOR_H_

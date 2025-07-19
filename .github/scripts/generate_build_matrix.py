b0114bd2f75541ed75d6a9d3c0811ca9d8c1539e
Fangjun Kuang <csukuangfj@gmail.com>
2025-07-19 10:47:26 +0800

    refactor CI

---
+++ .github/scripts/generate_build_matrix.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+import json
+
+
+def main():
+    os_list = ["ubuntu-latest", "macos-latest"]
+    python_list = ["3.10"]
+    ans = {"os": os_list, "python-version": python_list}
+
+    print(f"matrix={json.dumps(ans)}")
+
+
+if __name__ == "__main__":
+    main()

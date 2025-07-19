bd011dd683c25977b623b0b5e3e554bf253a3486
Fangjun Kuang <csukuangfj@gmail.com>
2025-07-19 12:20:27 +0800

    fix mlx

--- sherpa-mlx/csrc/silero-vad-model.cc
+++ sherpa-mlx/csrc/silero-vad-model.cc
@@ -158,12 +158,11 @@ class SileroVadModel::Impl {
 
   void ResetV4() {
     if (states_.empty()) {
-      std::vector<float> tmp(64);
       states_.reserve(4);
       for (int32_t i = 0; i != 4; ++i) {
-        mx::array s(tmp.data(), {1, 64});
-        states_.push_back(std::move(s));
+        states_.push_back(mx::zeros({1, 64}, mx::float32));
       }
+      return;
     }
 
     for (auto &s : states_) {

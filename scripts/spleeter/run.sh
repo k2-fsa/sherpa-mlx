8ccce30edb55e3a359caf7acbdbafdf6e36794f3
Fangjun Kuang <csukuangfj@gmail.com>
2025-07-29 17:40:40 +0800

    Add CI

--- scripts/spleeter/run.sh
+++ scripts/spleeter/run.sh
@@ -10,6 +10,10 @@ if [ ! -f accompaniment.pt ]; then
   curl -SL -O https://huggingface.co/csukuangfj/spleeter-torch/resolve/main/2stems/accompaniment.pt
 fi
 
+if [ ! -f ./accompaniment.wav ]; then
+  curl -SL -O https://github.com/k2-fsa/sherpa-mlx/releases/download/source-separation-models/audio_example.wav
+fi
+
 python3 ./export.py --dtype float32
 python3 ./export.py --dtype float16
 python3 ./export.py --dtype bfloat16
